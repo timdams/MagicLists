3ea9fdea80568a927f6364f2bcb565a6a684d8f7
Tim Dams <dams.tim@telenet.be>
2023-03-20 14:45:21 +0100

    Update

--- MagicLists/Card.cs
+++ MagicLists/Card.cs
@@ -8,27 +8,36 @@ namespace MagicLists
         [JsonPropertyName("artist")]
         public string Artist { get; set; }
 
+
         [JsonPropertyName("convertedManaCost")]
         public float ConvertedManaCost { get; set; }
 
         public Identifiers identifiers { get; set; }
+
         [JsonPropertyName("manaCost")]
         public string ManaCost { get; set; }
+
         [JsonPropertyName("manaValue")]
         public float ManaValue { get; set; }
 
         [JsonPropertyName("name")]
         public string Name { get; set; }
+
         [JsonPropertyName("number")]
         public string Number { get; set; }
+
         [JsonPropertyName("rarity")]
         public string Rarity { get; set; }
+
         [JsonPropertyName("text")]
         public string Text { get; set; }
+
         [JsonPropertyName("type")]
         public string Type { get; set; }
+
         [JsonPropertyName("types")]
         public string[] Types { get; set; }
+
         [JsonPropertyName("flavorText")]
         public string FlavorText { get; set; }
 
